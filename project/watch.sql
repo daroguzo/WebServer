353f2784f9139352f20d0c3b2cea64a2eab3f35d
DESKTOP-F3DRVR1\Jinoo <kjw2122@naver.com>
2019-06-18 22:10:13 +0900

    공공데이터 요악 sql

---
+++ project/watch.sql
@@ -0,0 +1,7 @@
+set character_set_client=utf8;
+set character_set_results=utf8;
+set character_set_connection=utf8;
+set character_set_server=utf8;
+set character_set_database=utf8;
+create table watchdata(pno int(5), pname int(5), area int(5), 
+ sdate int(5), region int(5), tel int(5));
